a5d1931d768c6d082010ebebe7579b90d541865f
daRellll <darylljohngsabanal@gmail.com>
2026-06-05 21:52:52 +0800

    created the constructor for the attributes of the fan

--- the_fan_class_programm/the_fan_class.py
+++ the_fan_class_programm/the_fan_class.py
@@ -2,3 +2,9 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def __init__(self, speed, radius, color, on=False):
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.on = on
