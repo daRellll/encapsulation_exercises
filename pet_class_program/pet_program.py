17612ce02d8a9ba369dbdd004b959b805eb9513e
daRellll <darylljohngsabanal@gmail.com>
2026-06-05 23:34:41 +0800

    added the set animal age in pet_program

--- pet_class_program/pet_program.py
+++ pet_class_program/pet_program.py
@@ -6,4 +6,4 @@ first_pet.set_name(input("What is your pets name? : "))
 
 first_pet.set_animal_type(input("What is your pets animal type? : "))
 
-
+first_pet.set_age(int(input("What is your pets age? : ")))
