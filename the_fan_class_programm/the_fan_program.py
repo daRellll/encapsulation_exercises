2e3acaa62c1a71c4acdcfa1ee7224ca505a07e27
daRellll <darylljohngsabanal@gmail.com>
2026-06-05 22:39:18 +0800

    created the fan1 and a prnt to print their attributes and show their function

--- the_fan_class_programm/the_fan_program.py
+++ the_fan_class_programm/the_fan_program.py
@@ -1,3 +1,6 @@
 from the_fan_class import Fan
 
 fan1 = Fan(speed=Fan.FAST, radius=10, color="yellow", on=True)
+fan2 = Fan(speed=Fan.MEDIUM, radius=5, color="blue", on=False)
+
+print(f"Fan1; Speed: {fan1.get_speed()} Radius: {fan1.get_radius()} Color: {fan1.get_color()} Status: {fan1.get_on()}")
